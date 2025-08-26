9f063711ba47f95ae76a65a2c7025767aee0a938
Totoy7777 <elvio4706@gmail.com>
2025-08-26 12:41:58 -0500

    Prueba1

---
+++ Backend/tienda_adso/src/main/java/com/proyecto_final/tienda_adso/TiendaAdsoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_final.tienda_adso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAdsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAdsoApplication.class, args);
+	}
+
+}
---
+++ Backend/tienda_adso/src/test/java/com/proyecto_final/tienda_adso/TiendaAdsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_final.tienda_adso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAdsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
